604de542365423aac44a6ca4a12e8d5e50a633aa
Damian Soria <damian.soria@youbim.com>
2020-03-29 10:01:55 -0300

    feat: create folder docs

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -12,7 +12,7 @@ module.exports = (env) => {
         mode: 'production',
         output: {
             filename: 'js/app.bundle.js',
-            publicPath: '/'
+            path: path.resolve(__dirname, '../docs'),
         },
         optimization: {
             splitChunks: {
